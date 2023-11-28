7b61379e19f89014c055389057ab307c0e74d38f
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-11-28 09:13:27 +0530

    Create Find duplicate in the array.cpp

---
+++ Day17/Find duplicate in the array.cpp
@@ -0,0 +1,10 @@
+int findDuplicate(vector<int> &arr, int n){
+	map<int,int> mp;
+	for(int i = 0; i < n; i++){
+		mp[arr[i]]++;
+	}
+
+	for(auto &it : mp){
+		if(it.second > 1) return it.first;
+	}
+}
