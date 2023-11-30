5c9ee1b7a4e3e2c454ca51e62fcba2c86c180a93
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-11-30 12:55:54 +0530

    Create postorder traversal.cpp

---
+++ Day19/postorder traversal.cpp
@@ -0,0 +1,8 @@
+void postOrderTrav(node * curr, vector < int > & postOrder) {
+  if (curr == NULL)
+    return;
+
+  postOrderTrav(curr -> left, postOrder);
+  postOrderTrav(curr -> right, postOrder);
+  postOrder.push_back(curr -> data);
+}
