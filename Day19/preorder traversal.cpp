a9a9c6a0b4945f64578bd49243d0c0ccddbd369a
girirajbidwai <95566572+girirajbidwai@users.noreply.github.com>
2023-11-30 12:54:06 +0530

    Create preorder traversal.cpp

---
+++ Day19/preorder traversal.cpp
@@ -0,0 +1,8 @@
+void preOrderTrav(node * curr, vector < int > & preOrder) {
+  if (curr == NULL)
+    return;
+
+  preOrder.push_back(curr -> data);
+  preOrderTrav(curr -> left, preOrder);
+  preOrderTrav(curr -> right, preOrder);
+}
